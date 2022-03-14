c3aee1f34ba962a6961382fa28333eb1466fabe2
rvaryanica <romavaryanitsa@yandex.ru>
2022-03-14 11:48:23 +0300

    first

---
+++ src/main/java/JavaBasicPractic.java
@@ -0,0 +1,37 @@
+
+public class JavaBasicPractic {
+
+    public static void main(String[] args) {
+        short s = 100;
+        long l = 600;
+        int a = 17, b = 12, c = 2;
+        byte q = 127;
+        boolean t = true, f = false;
+        String myName = "Roman";
+        char fChar = 'h', sChar = 'i';
+        double g = 2.0;
+        float z = 2, y = 4;
+
+        System.out.println(fChar + " " + sChar);
+        System.out.println(myName);
+        System.out.println(z + y);
+        System.out.println(a + l);
+        System.out.println(a + b);
+        System.out.println(a * c);
+        System.out.println(a / c);
+        System.out.println(a - c);
+        System.out.println(a % c);
+        System.out.println(++b);
+        System.out.println(--a);
+        System.out.println(a <= b);
+        System.out.println(a < b);
+        System.out.println(a > b);
+        System.out.println(a >= b);
+        System.out.println(a == b);
+        System.out.println((a > b) || (c > b));
+        System.out.println((a > b) && (c > b));
+        System.out.println((a > b) && f);
+        System.out.println(t || (c > b));
+
+    }
+}
